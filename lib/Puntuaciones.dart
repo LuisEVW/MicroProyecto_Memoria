060812d6120eb2186d273ee346a887b93d84f784
LuisEVW <luisviloria@correo.unimet.edu.ve>
2026-02-03 18:40:39 -0400

    UI inicial de puntuaciones

--- lib/Puntuaciones.dart
+++ lib/Puntuaciones.dart
@@ -10,6 +10,50 @@ class Puntuaciones extends StatefulWidget {
 class _PuntuacionesState extends State<Puntuaciones> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Mejor puntuación:',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              '[[[AQUÍ IRÁ LA MEJOR PUNTUACIÓN]]]',
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 40),
+            SizedBox(
+              width: 300,
+              height: 60,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PantallaPrincipal(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepOrange,
+                ),
+                child: Text(
+                  "Volver a la Pantalla Principal",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
